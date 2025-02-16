5846dd6bfbd630e654be835c535b81d9888fcff6
acer1 <Smirnovsd2405@yandex.ru>
2025-02-16 16:33:28 +0400

    main 2

---
+++ new.py
@@ -0,0 +1,5 @@
+i = 5
+print(f"self.num{i} = QLabel(self)")
+print(f"self.num{i}.move(20, 160)")
+print(f"self.num5.setText('Задание 5')")
+print(f"self.pixma5 = QPixmap(f'tasks\\5\\{self.num_opt[4]}.png')")
